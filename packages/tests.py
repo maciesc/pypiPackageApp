8257a5aeb0c7db95c1101636bd9e85f1fa2f3156
Maciej Ciupa <maciekc1996@gmail.com>
2022-09-29 19:39:09 +0200

    added create_save package model test

--- packages/tests.py
+++ packages/tests.py
@@ -1,3 +1,25 @@
+from datetime import datetime
+
 from django.test import TestCase
 
-# Create your tests here.
+from packages.models import Package
+
+
+class PackageTestCase(TestCase):
+    def test_creates_and_save_package_from_dict_data(self):
+        package = {
+            "title": "nonebot-plugin-lisenter added to PyPI",
+            "link": "https://pypi.org/project/nonebot-plugin-lisenter/",
+            "guid": "https://pypi.org/project/nonebot-plugin-lisenter/",
+            "description": "test_description",
+            "author": "test_author",
+            "pubdate": datetime(2022, 9, 29, 17, 19, 43),
+            "name": "nonebot-plugin-lisenter",
+        }
+        p = Package(**package)
+        p.save()
+
+        for key in package.keys():
+            assert getattr(p, key) == package[key]
+
+        assert Package.objects.count() == 1
